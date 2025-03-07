cf07cb32d7abe72b52d431e178bd78a95bbd7cca
Alpha <121796109+Alphastranger@users.noreply.github.com>
2025-03-07 14:23:01 -0500

    Many functions and questions

---
+++ 7-Wonders.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// Clock
+int clockDown = 154;
+// Resources:
+int gold;
+int stone;
+int wood;
+int energy;
+// Stats:
+string charName;
+int leadership;
+int leaderMod = (leadership/1.5)*10;
+int intelligence;
+int intelMod = (intelligence/3)*10;
+int cunning;
+int cunningMod = (cunning/2)*10;
+int endurance;
+int enduranceMod = (endurance/4)*10;
+// Wonders:
+class Wonders {
+    public:
+    string name;
+    int stoneCost;
+    int woodCost;
+};
+// Characters:
+class Characters {
+    public:
+    string name;
+    int startLead;
+    int startInt;
+    int startCun;
+    int startEnd;
+    int startGold;
+    int startStone;
+    int startWood;
+    int startEnergy;
+};
+
+void characterSelect(){
+    string charNameInput;
+    cout << "Select your character" << "\n";
+    cin >> charNameInput;
+    if (charNameInput == "Hadrian" || charNameInput == "hadrian"){
+        hadrianStart();
+    } else {
+        cout << "Please put in the correct name"<< "\n";
+        characterSelect();
+    };
+};
+
+void hadrianStart(){
+    Characters Hadrian;
+        Hadrian.name = "Hadrian";
+        Hadrian.startLead = 20;
+        Hadrian.startInt = 10;
+        Hadrian.startCun = 5;
+        Hadrian.startEnd = 10;
+        Hadrian.startGold = 300;
+        Hadrian.startStone = 300;
+        Hadrian.startWood = 100;
+        Hadrian.startEnergy = 10;
+    charName = Hadrian.name;
+    leadership = Hadrian.startLead;
+    intelligence = Hadrian.startInt;
+    cunning = Hadrian.startCun;
+    endurance = Hadrian.startEnd;
+    gold = Hadrian.startGold;
+    stone = Hadrian.startStone;
+    wood = Hadrian.startWood;
+    energy = Hadrian.startEnergy;
+    //Later we want energy to be calculated off the endurance stat
+    cout << "Ave imperator Emperor Hadrian! Welcome to 7 Wonders!"<<"\n";
+    startScreen();
+};
+void startScreen(){
+  
+       
+        cout << charName << "\n" << "\n";
+        cout << "Resources:   " << "Stats:" << "\n";
+        cout << "Gold:" << gold << " Leadership:" << leadership << "\n";
+        cout << "Stone:" << stone << " Intelligence:" << intelligence << "\n";
+        cout << "Wood:" << wood << " Cunning:" << cunning << "\n";
+        cout << "Energy:" << energy << " Endurance:" << endurance << "\n";
+        questionsFunc();
+    };
+//Questions
+class Questions {
+    public:
+    string questionText;
+    void resourceUp(int source, int Inc, int Mod){
+        source + Inc + Mod;
+    };
+    void resourceDown(int source, int Dec){
+        source - Dec;
+    }
+    void statUp(int source, int Inc){
+        source + Inc;
+    }
+    void statDown(int source, int Dec){
+        source - Dec;
+    }
+    // Questions (string x, int y, int w, int z){
+    //     questionText = x;
+    //     resourceUp = y;
+    //     resourceDown = w;
+    //     statChange = z;
+    // }
+};
+void questionsFunc(){ 
+    char answerVar;
+        Questions questZero;
+            questZero.questionText = "Spend 20 stone to get 50 gold?";
+            questZero.resourceUp(gold, 50, cunningMod);
+            questZero.resourceDown(stone, 20);
+        Questions questOne;
+            questOne.questionText = "Spend 10 energy to mine 100 stone?";
+            questOne.resourceUp(stone, 100, leaderMod);
+            questOne.resourceDown(energy, 10);
+        Questions questTwo;
+            questTwo.questionText = "Spend 10 energy to harvest 100 wood?";
+            questTwo.resourceUp(wood, 100, intelMod);
+            questTwo.resourceDown(energy, 10);
+        Questions questThree;
+            questThree.questionText = "Spend 120 gold to get 200 wood?";
+            questThree.resourceUp(wood, 200, intelMod);
+            questThree.resourceDown(gold, 120);
+        Questions questFour;
+            questFour.questionText = "Sacrifice 10 leadership for 300 gold?";
+            questFour.resourceUp(gold, 300, cunningMod);
+            questFour.statDown(leadership, 10);
+        Questions questFive;
+            questFive.questionText ="Spend 100 gold for 5 energy?";
+            questFive.resourceUp(energy, 5, enduranceMod);
+            questFive.resourceDown(gold, 100);
+        Questions questSix;
+            questSix.questionText ="Would you like to buy a 15 cunning boost for 500 gold?";
+            questSix.resourceDown(gold, 500);
+            questSix.statUp(cunning,15);
+
+
+    Questions qArray[7] = {
+        questZero,
+        questOne,
+        questTwo,
+        questThree,
+        questFour,
+        questFive,
+        questSix
+    };
+
+        Wonders pyramids;
+            pyramids.name = "Pyramids";
+            pyramids.stoneCost = 800;
+            pyramids.woodCost = 800;
+
+    int randomNum1 = rand()%7-1;
+    int randomNum2 = rand()%7-1;
+    if (randomNum2 == randomNum1 && randomNum2 !=0) {
+        randomNum2-1;
+    } else if (randomNum2 == randomNum1 && randomNum2 !=6) {
+        randomNum2+1;
+    }
+    int randomNum3 = rand()%7-1;
+    if (randomNum3 == randomNum1 || randomNum3 == randomNum2 && randomNum3>=2){
+        randomNum3-2;
+    } else if (randomNum3 == randomNum1 || randomNum3 == randomNum2 && randomNum3<=4){
+        randomNum3+2;
+    }
+    cout << "A. " << qArray[randomNum1].questionText << "\n";
+    cout << "B. " << qArray[randomNum2].questionText << "\n";
+    cout << "C. " << qArray[randomNum3].questionText << "\n";
+    cout << "D. " << pyramids.name << " Stone:" << pyramids.stoneCost << " Wood:" << pyramids.woodCost << "\n";
+    cout <<"Please input the associated letter with your answer."<<"\n";
+        cin >> answerVar;
+        if (answerVar == 'A' || answerVar == 'a') {
+
+        }
+
+        else {
+            cout << "Invalid input. Please enter a valid answer."<< "\n";
+            cin.clear();
+        }
+        
+        energy + endurance;
+
+        clockDown - 7;
+        if (clockDown > 0) {
+            startScreen();
+        } else {
+            cout << "Game Over" << "\n";
+        }
+        
+};
+
+
+
+
+void runProgram(){
+    
+};
+
+int main(){
+    runProgram();
+    return 0;
+};
